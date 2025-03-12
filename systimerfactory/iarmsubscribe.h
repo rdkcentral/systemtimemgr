88471559eaaa14edb81594919db5c7d04d8303d8
Ramesh Babu H <rababu2@synamedia.com>
2025-03-12 14:54:00 +0530

    systimermgr Powercontroller changes, power_connect added, powerconnect retryy, cb event handler changes

--- systimerfactory/iarmsubscribe.h
+++ systimerfactory/iarmsubscribe.h
@@ -19,25 +19,59 @@
 #define _IARMSUBSCRIBER_H_
 
 #include <string>
+#include <queue>
+#include <atomic>
+#include <thread>
+#include <condition_variable>
+#include <unistd.h>
 #include "isubscribe.h"
+#include "power_controller.h"
 
 //Although we could use  Lambda functions and higher order functions(which are available from c++11 onwards.)
 //Since the registration functions typically c functions. 
 //Perhaps this can be cosidered at a later point of time.
 
 using namespace std;
+
+/* System Time Mgr Power Controller State Data Structure to Pass Queue Data to the Event handler Thread */
+typedef struct SysTimeMgr_Power_Event_State{
+    PowerController_PowerState_t currentState;
+    PowerController_PowerState_t newState;
+    SysTimeMgr_Power_Event_State(PowerController_PowerState_t currSt, PowerController_PowerState_t newSt)
+        : currentState(currSt), newState(newSt) {}
+}SysTimeMgr_Power_Event_State_t;
+
+/* power controller connect function retry delay of 300msec */
+#define     PWR_CNTRL_CONNECT_WAIT_TIME_MS     (300000)
+
 class IarmSubscriber:public ISubscribe
 {
-	private:
-		static IarmSubscriber* pInstance;
-		funcPtr m_powerHandler;
+         private:
+            static IarmSubscriber* pInstance;
+            funcPtr m_powerHandler;
+            /* Power Controller queue and mutex to guard it*/
+            static std::queue<SysTimeMgr_Power_Event_State_t> m_pwrEvtQueue;
+            static std::mutex m_pwrEvtQueueLock;
+            /* Power Controller Event Handling Thread variables*/
+            static std::thread m_sysTimeMgrPwrEvtHandlerThread;
+            static std::mutex m_pwrEvtMutexLock;
+            static std::condition_variable m_pwrEvtCondVar;
+            static std::atomic<bool> m_sysTimeMgrPwrStopThread;
+
 	public:
 		IarmSubscriber(string sub);
 		bool subscribe(string eventname,funcPtr fptr);
 		static IarmSubscriber* getInstance() { return pInstance;}
 		void invokepowerhandler(void* args){ if (m_powerHandler) (*m_powerHandler)(args);}
-		static void powereventHandler(const char *owner, int eventId, void *data, size_t len);
+            static void sysTimeMgrPwrEventHandler(const PowerController_PowerState_t currentState,
+                                                                         const PowerController_PowerState_t newState,
+                                                                         void* userdata);
+            static void sysTimeMgrHandlePwrEventData(const PowerController_PowerState_t currentState,
+                            const PowerController_PowerState_t newState);
+            static void sysTimeMgrPwrEventHandlingThreadFunc(void *arg);
+            static void sysTimeMgrPwrConnectHandlingThreadFunc(void *arg);
+            static void sysTimeMgrInitPwrEvt(void);
+            static void sysTimeMgrDeinitPwrEvt(void);
 };
-
 
 #endif// _IARMSUBSCRIBER_H_
