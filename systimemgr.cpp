b7d42727b30f245fc4c3139b626f51b3ce39716f
Vismal S Kumar <ajvismal@yahoo.com>
2025-05-03 00:07:18 +0530

    Update systimemgr.cpp

--- systimemgr.cpp
+++ systimemgr.cpp
@@ -62,8 +62,10 @@ SysTimeMgr::SysTimeMgr (string cfgfile):m_state(eSYSMGR_STATE_INIT),
 			  m_timersrc("Last Known"),
                           m_publish(NULL),
 			  m_subscriber(NULL),
+<<<<<<< HEAD
 			  m_tmrsubscriber(NULL),
-			  m_cfgfile(cfgfile)
+			  m_cfgfile(std::move(cfgfile))
+
 {
 }
 
