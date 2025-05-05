3d800e89d97c4f41e4094d3195a59145c7e59dbd
Ramesh Babu H <rababu2@synamedia.com>
2025-05-05 13:57:55 +0530

    RDK-56904: Systemtimemgr - header changes retained as in existing files

--- systimerfactory/iarmtimerstatussubscriber.h
+++ systimerfactory/iarmtimerstatussubscriber.h
@@ -1,8 +1,5 @@
 /*
- * If not stated otherwise in this file or this component's LICENSE file the
- * following copyright and licenses apply:
- *
- * Copyright 2025 RDK Management
+ * Copyright 2023 Comcast Cable Communications Management, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #ifndef _IARMTIMERSTATUSSUBSCRIBER_H_
 #define _IARMTIMERSTATUSSUBSCRIBER_H_
 
