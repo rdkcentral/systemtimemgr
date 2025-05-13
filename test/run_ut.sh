cfa16f82e6c0f0d25ef6650c8e4567c4f9aadc09
nhanasi <navihansi@gmail.com>
2025-05-13 16:00:44 -0400

    Update run_ut.sh

--- test/run_ut.sh
+++ test/run_ut.sh
@@ -59,7 +59,7 @@ if [ "$ENABLE_COV" = true ]; then
     echo "Generating coverage report"
     lcov --capture --directory . --output-file coverage.info
     lcov --remove coverage.info '/usr/*' --output-file coverage.info
-    lcov --remove coverage.info '/systimerfactory/unittest/*' --output-file coverage.info
+    lcov --remove coverage.info './systimerfactory/unittest/*' --output-file coverage.info
     lcov --list coverage.info
 fi
 
