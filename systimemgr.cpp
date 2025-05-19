5bd5b27a7769def4f2045c73b6f136a892c0b46d
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2025-05-19 13:56:25 +0530

    Update systimemgr.cpp

--- systimemgr.cpp
+++ systimemgr.cpp
@@ -33,7 +33,7 @@
 #include "itimermsg.h"
 #include <chrono>
 #include "secure_wrapper.h"
-#include "rdkdefaulttimesync.h"
+#include "../systimemgrfactory/rdkdefaulttimesync.h"
 using namespace std::chrono;
 
 
