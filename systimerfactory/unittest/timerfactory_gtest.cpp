7641a1fe1912102098cfa80dac14cd4cb23aaa39
Vismal S Kumar <ajvismal@yahoo.com>
2025-05-26 16:04:40 +0530

    Update timerfactory_gtest.cpp

--- systimerfactory/unittest/timerfactory_gtest.cpp
+++ systimerfactory/unittest/timerfactory_gtest.cpp
@@ -4,6 +4,12 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <string>
+
+#include "Client_Mock.h"
+//print the Debug Lines on the console remove rdklogger dependency
+#define _IRDKLOG_H_
+#define RDK_LOG(level, module, format, ...) printf("[%s:%s]" format, #level, #module, __VA_ARGS__)
+
 #include "timerfactory.h"
 #include "timerfactory.cpp"
 using namespace std;
