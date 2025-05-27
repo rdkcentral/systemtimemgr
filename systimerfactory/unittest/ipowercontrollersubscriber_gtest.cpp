10dedc9751fe551a3ad141f0d95bd9a2be9fe97c
Vismal S Kumar <ajvismal@yahoo.com>
2025-05-27 12:29:06 +0530

    Update ipowercontrollersubscriber_gtest.cpp

--- systimerfactory/unittest/ipowercontrollersubscriber_gtest.cpp
+++ systimerfactory/unittest/ipowercontrollersubscriber_gtest.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include <iostream>
-#include "powercontroller.h"
+#include "power_controller.h"
 #include "ipowercontrollersubscriber.h"
 #include "ipowercontrollersubscriber.cpp"
 
