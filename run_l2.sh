01f588fe6e12c6ef5d8ffaae35bf4348146596da
Saranya2421 <saranya.suvi@gmail.com>
2025-06-27 11:20:19 +0530

    Update run_l2.sh

--- run_l2.sh
+++ run_l2.sh
@@ -28,17 +28,21 @@ ln -s /usr/local/bin/journalctl /usr/bin/journalctl
 
 rm -rf /opt/logs/systimemgr.log*
 
-/usr/local/bin/sysTimeMgr &
+#/usr/local/bin/sysTimeMgr &
 
-ps -eaf
-ls -l /opt/logs/
+#ps -eaf
+#ls -l /opt/logs/
 
-cat /opt/logs/systimemgr.log.0
-cat /etc/debug.ini
-sleep 1m
+#cat /opt/logs/systimemgr.log.0
+#cat /etc/debug.ini
+#sleep 1m
 
 # Run L2 Test cases
 pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/systimemgr_single_instance.json test/functional-tests/tests/test_systimemgr_single_instance.py
+
+ps -eaf
+ls -l /opt/logs
+cat /opt/logs/systimemgr.log.0
 pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/systimemgr_initialisation.json test/functional-tests/tests/test_systimemgr_initialisation.py
 #pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/systimemgr_get_time.json test/functional-tests/tests/test_systimemgr_get_time.py
 #pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/systimemgr_check_file.json test/functional-tests/tests/test_systimemgr_check_file.py
