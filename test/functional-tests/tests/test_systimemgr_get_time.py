4cbd2bead754cf8a94c156a53fbf961d4c4d0900
Saranya2421 <saranya.suvi@gmail.com>
2025-06-27 14:33:27 +0530

    Update test_systimemgr_get_time.py

--- test/functional-tests/tests/test_systimemgr_get_time.py
+++ test/functional-tests/tests/test_systimemgr_get_time.py
@@ -19,14 +19,10 @@
 from time import sleep
 from helper_functions import *
 
-def test_trigger_systimemgr_instance():
-    kill_sysTimeMgr()
-    print("Starting systemtimemgr process")
-    command_to_start = "nohup /usr/local/bin/sysTimeMgr > /dev/null 2>&1 &"
-    run_shell_silent(command_to_start)
-    command_to_get_pid = "pidof sysTimeMgr"
-    pid = run_shell_command(command_to_get_pid)
-    assert pid != "", "sysTimeMgr process did not start"
+def test_is_systimemgr_running():
+    command_to_check = "pidof sysTimeMgr"
+    result = run_shell_command(command_to_check)
+    assert result != "", "sysTimeMgr process did not start"
 
 def test_if_systimemgr_running():
     command_to_check = "ps aux | grep sysTimeMgr | grep -v grep"
