5a307666e53400b4e6a5f84e392b457eda26371d
Saranya2421 <saranya.suvi@gmail.com>
2025-06-27 10:01:50 +0530

    Update run_l2.sh

--- run_l2.sh
+++ run_l2.sh
@@ -28,7 +28,7 @@ ln -s /usr/local/bin/journalctl /usr/bin/journalctl
 
 rm -rf /opt/logs/systimemgr.log*
 
-/usr/local/bin/sysTimeMgr -d /etc/debug.ini | tee /opt/logs/systimemgr.log.0 &\
+/usr/local/bin/sysTimeMgr -d /etc/debug.ini | tee /opt/logs/systimemgr.log.0 &
 
 # Run L2 Test cases
 #pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/systimemgr_single_instance.json test/functional-tests/tests/test_systimemgr_single_instance.py
