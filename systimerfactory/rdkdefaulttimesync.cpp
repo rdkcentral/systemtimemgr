73c16f02047b49ce69a4aebef6f441891dab8b44
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2025-07-04 14:49:01 +0530

    Update rdkdefaulttimesync.cpp

--- systimerfactory/rdkdefaulttimesync.cpp
+++ systimerfactory/rdkdefaulttimesync.cpp
@@ -21,6 +21,17 @@
 #include <chrono>
 #include <telemetry_busmessage_sender.h>
 
+#ifdef T2_EVENT_ENABLED
+void t2CountNotify(const char *marker, int val) {
+    t2_event_d(marker, val);
+}
+
+void t2ValNotify( const char *marker, const char *val )
+{
+    t2_event_s(marker, val);
+}
+#endif
+
 using namespace std::chrono;
 map<string, string> RdkDefaultTimeSync::tokenize(string const& s,string token)
 {
@@ -99,7 +110,7 @@ long long RdkDefaultTimeSync::getTime()
 		RDK_LOG(RDK_LOG_INFO,LOG_SYSTIME,"[%s:%d]:Returning Last Known Good Time, time = %s \n",__FUNCTION__,__LINE__,timeStr);
 		char buffer[128];
                 snprintf(buffer, 128, "Returning Last Known Good Time, time = %s", timeStr);
-                t2_event_s("SYST_INFO_SYSLKG",buffer);
+                t2ValNotify("SYST_INFO_SYSLKG",buffer);
 		m_currentTime = clock_time;
 		return clock_time;
 	}
@@ -110,7 +121,7 @@ long long RdkDefaultTimeSync::getTime()
 	RDK_LOG(RDK_LOG_INFO,LOG_SYSTIME,"[%s:%d]:Returning build time, Time = %s\n",__FUNCTION__,__LINE__,timeStr);
 	char buffer[128];
         snprintf(buffer, 128, "Returning build time, Time = %s", timeStr);
-        t2_event_s("SYST_INFO_SYSBUILD",buffer);
+        t2ValNotify("SYST_INFO_SYSBUILD",buffer);
 	return ver_time;
 }
 
