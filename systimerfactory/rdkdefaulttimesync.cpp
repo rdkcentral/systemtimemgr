b07e3fd01a2b84f6a807d88e6dc38537e2f30968
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2025-07-08 15:05:20 +0530

    Update rdkdefaulttimesync.cpp

--- systimerfactory/rdkdefaulttimesync.cpp
+++ systimerfactory/rdkdefaulttimesync.cpp
@@ -112,7 +112,7 @@ long long RdkDefaultTimeSync::getTime()
 		RDK_LOG(RDK_LOG_INFO,LOG_SYSTIME,"[%s:%d]:Returning Last Known Good Time, time = %s \n",__FUNCTION__,__LINE__,timeStr);
 		char buffer[128];
                 snprintf(buffer, 128, "Returning Last Known Good Time, time = %s", timeStr);
-		t2ValNotify("SYST_INFO_SYSLKG",buffer);
+		t2ValNotify(const_cast<char*>("SYST_INFO_SYSLKG"),const_cast<char*>(buffer));
                  t2_event_s("SYST_INFO_SYSLKG",buffer);
 		m_currentTime = clock_time;
 		return clock_time;
@@ -124,7 +124,7 @@ long long RdkDefaultTimeSync::getTime()
 	RDK_LOG(RDK_LOG_INFO,LOG_SYSTIME,"[%s:%d]:Returning build time, Time = %s\n",__FUNCTION__,__LINE__,timeStr);
 	char buffer[128];
         snprintf(buffer, 128, "Returning build time, Time = %s", timeStr);
-         t2ValNotify("SYST_INFO_SYSBUILD",buffer);
+         t2ValNotify(const_cast<char*>("SYST_INFO_SYSBUILD"),const_cast<char*>(buffer));
 	return ver_time;
 }
 
