6bd3aff28d15da96bc3d3ac6d3901fec71da19c9
Vismal S Kumar <ajvismal@yahoo.com>
2025-07-18 15:43:29 +0530

    Update ipowercontrollersubscriber.h

--- systimerfactory/ipowercontrollersubscriber.h
+++ systimerfactory/ipowercontrollersubscriber.h
@@ -59,7 +59,10 @@ class IpowerControllerSubscriber:public IarmSubscriber
 		std::condition_variable m_pwrEvtCondVar;
 
 	public:
-		IpowerControllerSubscriber(string sub);
+		IpowerControllerSubscriber(string #ifdef UNIT_TEST
+    // Test-specific constructor with shutdown control
+    IpowerControllerSubscriber(std::string sub, bool testMode);
+#endif
 		bool subscribe(string eventname,funcPtr fptr) override;
 		void invokepowerhandler(void* args){ if (m_powerHandler) (*m_powerHandler)(args);}
 		static void sysTimeMgrPwrEventHandler(const PowerController_PowerState_t currentState,
