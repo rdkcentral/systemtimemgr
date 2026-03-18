c7c23b6a57eb89452db5d60aa1e8cdc547743cde
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-03-18 13:57:41 +0530

    Update systimemgr.cpp

--- systimemgr.cpp
+++ systimemgr.cpp
@@ -1,4 +1,4 @@
-/*
+F/*
  * Copyright 2023 Comcast Cable Communications Management, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -643,6 +643,7 @@ void SysTimeMgr::deepsleepoff()
 		if (ret == 0) {
             // chronyd is running
             RDK_LOG(RDK_LOG_INFO,LOG_SYSTIME,"[%s:%d]:chronyd is active, performing chronyc makestep\n",__FUNCTION__,__LINE__);
+			v_secure_system("/usr/sbin/chronyc burst 2/4");
             v_secure_system("/usr/sbin/chronyc makestep");
         } else {
             RDK_LOG(RDK_LOG_WARN,LOG_SYSTIME,"[%s:%d]:Neither systemd-timesyncd nor chronyd is running, skipping time sync actions.\n",__FUNCTION__,__LINE__);
