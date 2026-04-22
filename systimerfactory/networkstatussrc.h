15e706d0d8262c92488b7a1265505f9e899d7243
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-04-22 15:16:04 +0530

    Update networkstatussrc.h

--- systimerfactory/networkstatussrc.h
+++ systimerfactory/networkstatussrc.h
@@ -36,7 +36,7 @@ class NetworkStatusSrc
                ~NetworkStatusSrc();
                 void subscribeToInternetEvent();
                 void unsubscribeFromInternetEvent();
-                static void plugin_statechange(const JsonObject& parameters);
+                void plugin_statechange(const JsonObject& parameters);
 };
 
 #endif // _NETWORKSTATUSSRC_H_
